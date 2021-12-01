20083d3f33be4ce4ca68cf048d89c31f9e21c214
Marcin Kołodziej <dev@izoslav.pl>
2021-12-02 00:59:56 +0100

    rustfmt aoc 2018 day 8

--- aoc2018/src/bin/day08.rs
+++ aoc2018/src/bin/day08.rs
@@ -2,63 +2,63 @@ use std::fs;
 use std::slice::Iter;
 
 fn main() {
-  let input: Vec<usize> = fs::read_to_string("res/day08_1.txt")
-    .unwrap()
-    .split_ascii_whitespace()
-    .map(|entry| entry.parse::<usize>().unwrap())
-    .collect();
+    let input: Vec<usize> = fs::read_to_string("res/day08_1.txt")
+        .unwrap()
+        .split_ascii_whitespace()
+        .map(|entry| entry.parse::<usize>().unwrap())
+        .collect();
 
-  let mut iter = input.iter();
-  let sum = sum_metadata(&mut iter);
-  println!("Part 1 answer: {:?}", sum);
+    let mut iter = input.iter();
+    let sum = sum_metadata(&mut iter);
+    println!("Part 1 answer: {:?}", sum);
 
-  let mut iter = input.iter();
-  let indexed_sum = sum_indexed_metadata(&mut iter);
-  println!("Part 2 answer: {:?}", indexed_sum)
+    let mut iter = input.iter();
+    let indexed_sum = sum_indexed_metadata(&mut iter);
+    println!("Part 2 answer: {:?}", indexed_sum)
 }
 
 fn sum_metadata(iter: &mut Iter<usize>) -> usize {
-  let mut sum = 0;
+    let mut sum = 0;
 
-  let c = iter.next().unwrap();
-  let m = iter.next().unwrap();
+    let c = iter.next().unwrap();
+    let m = iter.next().unwrap();
 
-  for _ in 0..*c {
-    sum += sum_metadata(iter);
-  }
+    for _ in 0..*c {
+        sum += sum_metadata(iter);
+    }
 
-  for _ in 0..*m {
-    sum += *iter.next().unwrap();
-  }
+    for _ in 0..*m {
+        sum += *iter.next().unwrap();
+    }
 
-  sum
+    sum
 }
 
 fn sum_indexed_metadata(iter: &mut Iter<usize>) -> usize {
-  let c = iter.next().unwrap();
-  let m = iter.next().unwrap();
+    let c = iter.next().unwrap();
+    let m = iter.next().unwrap();
 
-  let mut children_values = vec![];
-  for _ in 0..*c {
-    children_values.push(sum_indexed_metadata(iter));
-  }
-
-  let mut metadata = vec![];
-  for _ in 0..*m {
-    metadata.push(*iter.next().unwrap());
-  }
-
-  if *c == 0 {
-    metadata.iter().sum()
-  } else {
-    let mut sum = 0;
-    for i in 0..metadata.len() {
-      let index = metadata[i] - 1;
-      if index < children_values.len() {
-        sum += children_values[index];
-      }
+    let mut children_values = vec![];
+    for _ in 0..*c {
+        children_values.push(sum_indexed_metadata(iter));
     }
-  
-    sum
-  }
+
+    let mut metadata = vec![];
+    for _ in 0..*m {
+        metadata.push(*iter.next().unwrap());
+    }
+
+    if *c == 0 {
+        metadata.iter().sum()
+    } else {
+        let mut sum = 0;
+        for i in 0..metadata.len() {
+            let index = metadata[i] - 1;
+            if index < children_values.len() {
+                sum += children_values[index];
+            }
+        }
+
+        sum
+    }
 }
