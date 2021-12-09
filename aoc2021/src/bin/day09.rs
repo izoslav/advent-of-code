c3bd255db3675c9c74338b5ea72d9981673a8860
Marcin Kołodziej <dev@izoslav.pl>
2021-12-09 18:58:42 +0100

    clippy aoc 2021 day 9

--- aoc2021/src/bin/day09.rs
+++ aoc2021/src/bin/day09.rs
@@ -6,7 +6,7 @@ use itertools::Itertools;
 fn main() {
     let input = fs::read_to_string("res/day09.txt").unwrap();
 
-    let width = input.lines().nth(0).unwrap().len();
+    let width = input.lines().next().unwrap().len();
     let height = input.lines().count();
 
     let mut heightmap = Array2D::filled_with(9, height + 2, width + 2);
@@ -15,7 +15,7 @@ fn main() {
         for (x, n) in line
             .trim()
             .chars()
-            .map(|n| usize::from_str_radix(&n.to_string(), 10).unwrap())
+            .map(|n| n.to_string().parse::<usize>().unwrap())
             .enumerate()
         {
             *heightmap.get_mut(y + 1, x + 1).unwrap() = n;
@@ -27,14 +27,14 @@ fn main() {
 
     for y in 1..height + 1 {
         for x in 1..width + 1 {
-            let n = *heightmap.get(y - 1, x).unwrap();
-            let s = *heightmap.get(y + 1, x).unwrap();
-            let w = *heightmap.get(y, x - 1).unwrap();
-            let e = *heightmap.get(y, x + 1).unwrap();
-            let c = *heightmap.get(y, x).unwrap();
+            let north = *heightmap.get(y - 1, x).unwrap();
+            let south = *heightmap.get(y + 1, x).unwrap();
+            let west = *heightmap.get(y, x - 1).unwrap();
+            let east = *heightmap.get(y, x + 1).unwrap();
+            let current = *heightmap.get(y, x).unwrap();
 
-            if [n, s, w, e].iter().all(|&p| c < p) {
-                risk_level += c + 1;
+            if [north, south, west, east].iter().all(|&p| current < p) {
+                risk_level += current + 1;
                 basins.push(check_basin(&mut heightmap, x, y));
             }
         }
@@ -45,7 +45,7 @@ fn main() {
         .sorted()
         .rev()
         .take(3)
-        .fold(1, |acc, &e| acc * e);
+        .product::<usize>();
 
     println!("Part 1 answer: {}", risk_level);
     println!("Part 2 answer: {}", mul_area);
@@ -58,10 +58,10 @@ fn check_basin(heightmap: &mut Array2D<usize>, x: usize, y: usize) -> usize {
 
     *heightmap.get_mut(y, x).unwrap() = 9usize;
 
-    let n = check_basin(heightmap, x, y + 1);
-    let s = check_basin(heightmap, x, y - 1);
-    let e = check_basin(heightmap, x + 1, y);
-    let w = check_basin(heightmap, x - 1, y);
+    let north = check_basin(heightmap, x, y + 1);
+    let south = check_basin(heightmap, x, y - 1);
+    let east = check_basin(heightmap, x + 1, y);
+    let west = check_basin(heightmap, x - 1, y);
 
-    1 + n + s + e + w
+    1 + north + south + east + west
 }
